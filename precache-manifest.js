77534e1ab2e2c7a3ee83497c9caec63c5ef13100
arishboteco <arish@boteco.co.in>
2025-08-04 04:20:07 -0700

    Use build-generated asset manifest

---
+++ precache-manifest.js
@@ -0,0 +1,18 @@
+self.__ASSETS_MANIFEST = [
+  "/",
+  "/index.html",
+  "/bar-menu.html",
+  "/food-menu.html",
+  "/party-booking.html",
+  "/specials-menu.html",
+  "/assets/css/boteco_style.min.css",
+  "/assets/js/carousel-counter.min.js",
+  "/assets/js/events.min.js",
+  "/assets/js/fade-in.min.js",
+  "/assets/js/header.min.js",
+  "/assets/js/hero-video.min.js",
+  "/assets/js/menu-gallery.min.js",
+  "/assets/js/uniform-menu-heights.min.js",
+  "/assets/events/archive/archive.json",
+  "/assets/events/events.json"
+];
